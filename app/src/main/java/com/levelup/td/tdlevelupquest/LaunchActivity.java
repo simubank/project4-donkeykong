9f6275450c3594b8371aed24f1634dd635792cc7
Het Kataria <hetkataria1@gmail.com>
2018-07-10 16:49:26 -0400

    API call to uclassify

--- app/src/main/java/com/levelup/td/tdlevelupquest/LaunchActivity.java
+++ app/src/main/java/com/levelup/td/tdlevelupquest/LaunchActivity.java
@@ -1,13 +1,74 @@
 package com.levelup.td.tdlevelupquest;
 
+import android.app.Activity;
+import android.content.Context;
+
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 public class LaunchActivity extends AppCompatActivity {
+
+    TextView t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        String url = "https://api.uclassify.com/v1/uClassify/Topics/classify/?readKey=sXvXN9MJP0DE&text=macbook";
+        JsonCall(url);
+    }
+
+    public void JsonCall (String URL){
+        RequestQueue requestQueue = Volley.newRequestQueue(LaunchActivity.this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("ok",response.toString());
+                t1 = findViewById(R.id.textView);
+                t1.setText(response.toString());
+            }
+        },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                    }
+                }
+        );
+        requestQueue.add(jsonObjectRequest);
     }
 }
+
+
+
+
+
+
+
