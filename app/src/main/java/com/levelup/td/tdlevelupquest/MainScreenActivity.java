c7708f8a3cef2e2af1f3be25c998cfab2794ccd4
Jeffrey Zhang <jeffreysfllo24@gmail.com>
2018-07-20 18:52:47 -0400

    - Worked on recommendation flow, currently have to click lets go twice

--- app/src/main/java/com/levelup/td/tdlevelupquest/MainScreenActivity.java
+++ app/src/main/java/com/levelup/td/tdlevelupquest/MainScreenActivity.java
@@ -65,38 +65,11 @@ import static java.nio.file.Files.newBufferedReader;
  */
 
 public class MainScreenActivity extends AppCompatActivity {
-    private GeoDataClient mGeoDataClient;
-    private PlaceDetectionClient mPlaceDetectionClient;
-    private LatLngBounds mBounds;
-    private LatLng mLatLgn;
-    private AutocompleteFilter mPlaceFilter;
-    private GoogleMap mgoogle;
-    private MapFragment mMap;
-    private String apiResult;
-    private GoogleApiClient mGoogleApiClient;
-
-    public class Pair {
-        private Integer integer;
-        private Place place;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
-        Button btn = findViewById(R.id.pickerButton);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkThisOut();
-            }
-        });
-        Log.d("wow", "onCreate: wwwwww");
-        // Construct a GeoDataClient.
-        mGeoDataClient = Places.getGeoDataClient(this, null);
 
-        // Construct a PlaceDetectionClient.
-        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
 
         // TODO: Start using the Places API.z
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -104,129 +77,11 @@ public class MainScreenActivity extends AppCompatActivity {
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     1);
         }
-        NetworkHelper.getInstance().botAPIGetRequest("https://dev.botsfinancial.com/api/accounts/873a24c9-1852-432a-8185-fb6e94d52ad1_b59fad0d-24ea-464c-a4cb-c2c1ee9702d9/transactions",
-                getApplicationContext(), new APICallback() {
-                    @Override
-                    public void onResponse(boolean success, JSONObject object) {
-                        botAPIMostFrequent(object);
-                        Log.d("tsest","wewew");
-                    }
-                });
+
         LaunchActivity.initDrawer(MainScreenActivity.this,this);
+
     }
 
-    private void checkThisOut(){
-        int PLACE_PICKER_REQUEST = 1;
-        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-        try{
-            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
-        }catch (Exception e) {
-            Log.d("error", e.getMessage());
-        }
-    }
-    private void botAPIMostFrequent(JSONObject account){
-//        if(account != null){
-//            Log.d("json",account.toString());
-//        }
-    }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        BufferedReader reader;
-        HashMap<Integer,String> placeTypeMap = new HashMap<>();
-
-        try{
-            Context context = getApplicationContext();
-            reader = new BufferedReader(new InputStreamReader(context.getAssets().open("keyValueForGooglePlaces.txt")));
-
-            String line;
-
-            while((line = reader.readLine()) != null){
-                String value = line;
-                Integer key = Integer.parseInt(reader.readLine());
-
-//                Log.d("AAAAA ", "value  " + value + " key " + key);
-                placeTypeMap.put(key,value);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (requestCode == 1) {
-            if (resultCode == RESULT_OK) {
-                final Place place = PlacePicker.getPlace(data, this);
-
-                List<Integer> placeTypesList = place.getPlaceTypes();
-
-                final Integer pickedPlacePriceLevel = place.getPriceLevel();
-                String toastMsg = String.format("Place Name: %s", placeTypesList);
-                Log.d("wwe", "Place Category Index:"+place.getPlaceTypes());
-                Log.d("wwe", "Place ID:"+place.getId());
-                Log.d("wwe", "Place Price Level:"+place.getPriceLevel());
-                Log.d("wwe", "Place Location:"+place.getLatLng());
-                Log.d("wwe", "Place Rating:"+place.getRating());
-
-
-                Double NELat = place.getLatLng().latitude + 0.5;
-                Double NELong = place.getLatLng().longitude + 0.5;
-                Double SWLat = place.getLatLng().latitude - 0.5;
-                Double SWLong = place.getLatLng().longitude - 0.5;
-                LatLng NELatlng = new LatLng(NELat, NELong);
-                LatLng SWLatlng = new LatLng(SWLat, SWLong);
-                LatLngBounds newbounds = new LatLngBounds(SWLatlng,NELatlng);
-                Integer placeMainType = place.getPlaceTypes().get(0);
-
-                Task<AutocompletePredictionBufferResponse> results =
-                        mGeoDataClient.getAutocompletePredictions(placeTypeMap.get(placeMainType), newbounds,
-                                null);
-                results.addOnCompleteListener(new OnCompleteListener<AutocompletePredictionBufferResponse>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AutocompletePredictionBufferResponse> task) {
-                        AutocompletePredictionBufferResponse autocompletePredictions = task.getResult();
-                        Iterator<AutocompletePrediction> iterator = autocompletePredictions.iterator();
-                        List<Pair> displayedPlaces = new ArrayList<Pair>();
-
-                        while(iterator.hasNext()){
-                            AutocompletePrediction prediction = iterator.next();
-                            Log.d("wwe", "Query completed. Received " + prediction.getFullText(null)+ " predictions.");
-                            String placeID = prediction.getPlaceId();
-                            placeIDCreate(placeID,displayedPlaces);
-                        }
-                        int count = 0;
-                        List<Place> finalPlaces= new ArrayList<Place>();
-
-                        for(int i = 0; i < displayedPlaces.size(); ++i){
-                            if((displayedPlaces.get(i).integer <= pickedPlacePriceLevel)&&(count <= 3)){
-                                finalPlaces.add(displayedPlaces.get(i).place);
-                                ++count;
-                            }
-                        }
-                        autocompletePredictions.release();
-                    }
-                });
-
-                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    public void placeIDCreate(String placeID, final List<Pair> topPlaces){
-        Task<PlaceBufferResponse> placeResult = mGeoDataClient.getPlaceById(placeID);
-        placeResult.addOnCompleteListener(new OnCompleteListener<PlaceBufferResponse>() {
-            @Override
-            public void onComplete(@NonNull Task<PlaceBufferResponse> task) {
-                PlaceBufferResponse places = task.getResult();
-                final Place place = places.get(0);
-                int priceLevel = place.getPriceLevel();
-                Pair newPair = new Pair();
-                newPair.integer = priceLevel;
-                newPair.place = place;
-                topPlaces.add(newPair);
-                Log.d("wwe", "Place Category Index:"+place.getPlaceTypes());
-                Log.d("wwe", "Place ID:"+place.getId());
-                Log.d("wwe", "Place Price Level:"+place.getPriceLevel());
-                Log.d("wwe", "Place Location:"+place.getLatLng());
-                Log.d("wwe", "Place Rating:"+place.getRating());
-            }
-        });
-    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
