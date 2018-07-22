d9c60ac626e103080d33a906b3798e4135c8ba1d
Het Kataria <hetkataria1@gmail.com>
2018-07-22 13:57:07 -0400

    change goals UI fixes

--- app/src/main/java/com/levelup/td/tdlevelupquest/CreateGoalsActivity.java
+++ app/src/main/java/com/levelup/td/tdlevelupquest/CreateGoalsActivity.java
@@ -1,9 +1,11 @@
 package com.levelup.td.tdlevelupquest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class CreateGoalsActivity extends AppCompatActivity {
@@ -14,6 +16,7 @@ public class CreateGoalsActivity extends AppCompatActivity {
     CardView c2;
     CardView c3;
     CardView c4;
+    Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class CreateGoalsActivity extends AppCompatActivity {
         c2 = findViewById(R.id.card2);
         c3 = findViewById(R.id.card3);
         c4 = findViewById(R.id.card4);
+        done = findViewById(R.id.doneButton);
 
         iv.setOnClickListener(new View.OnClickListener() {
 
@@ -45,13 +49,19 @@ public class CreateGoalsActivity extends AppCompatActivity {
                     c4.setVisibility(View.VISIBLE);
                 }
             }
-
         });
 
+        done.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), card_display.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
-
-
-
-
-
 }
