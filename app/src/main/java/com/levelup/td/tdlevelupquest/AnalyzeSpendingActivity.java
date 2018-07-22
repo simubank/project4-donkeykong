eb02f08227b9c06e17ac245e001921acdd983b43
Michael T Chau <mtchau@uwaterloo.ca>
2018-07-21 20:05:07 -0400

    Bar Graph + new header and more

--- app/src/main/java/com/levelup/td/tdlevelupquest/AnalyzeSpendingActivity.java
+++ app/src/main/java/com/levelup/td/tdlevelupquest/AnalyzeSpendingActivity.java
@@ -17,8 +17,10 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
@@ -26,6 +28,7 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.location.places.Places;
@@ -43,12 +46,43 @@ import java.util.Map;
 public class AnalyzeSpendingActivity extends AppCompatActivity {
     private HashMap<String,TransactionObject> transactionsMap = new HashMap<String,TransactionObject>();
     private Double ogAmount =0.0;
+    private Boolean pieChartVisiable = true;
+    private PieChart pieChart;
+    private BarChart barChart;
     private ListView transactionListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analyze_spending);
         transactionListView = findViewById(R.id.analyzeTransactionList);
+        LayoutInflater inflater = getLayoutInflater();
+        ViewGroup header = (ViewGroup)inflater.inflate(R.layout.header_analyze_spend, transactionListView, false);
+        transactionListView.addHeaderView(header, null, false);
+        pieChart =  findViewById(R.id.analzyePieChart);
+        barChart = findViewById(R.id.analzyeBarGraph);
+        barChart.setVisibility(View.GONE);
+
+        Button pieGraphBtn = findViewById(R.id.switchToPieChartButton);
+        pieGraphBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pieChartVisiable = true;
+                pieChart.setVisibility(View.VISIBLE);
+                barChart.setVisibility(View.GONE);
+                setPieGraph();
+            }
+        });
+
+        Button barGraphBtn = findViewById(R.id.switchToBarChartButton);
+        barGraphBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pieChartVisiable = false;
+                pieChart.setVisibility(View.GONE);
+                barChart.setVisibility(View.VISIBLE);
+                setupBarGraph();
+            }
+        });
 
         LaunchActivity.initDrawer(AnalyzeSpendingActivity.this,this);
         NetworkHelper.getInstance().botAPIGetRequest("https://dev.botsfinancial.com/api/simulatedaccounts/873a24c9-1852-432a-8185-fb6e94d52ad1_b59fad0d-24ea-464c-a4cb-c2c1ee9702d9/simulatedtransactions",
@@ -86,7 +120,13 @@ public class AnalyzeSpendingActivity extends AppCompatActivity {
             TransactionMapAdapter adapter = new TransactionMapAdapter(transactionsMap, new ListViewCallback() {
                 @Override
                 public void onButtonClicked() {
-                    setPieGraph();
+                    if(pieChartVisiable){
+                        Log.d("ww", "pie");
+                        setPieGraph();
+                    }else {
+                        Log.d("ww", "bar");
+                        setupBarGraph();
+                    }
                     percentChange();
                 }
             });
@@ -106,9 +146,60 @@ public class AnalyzeSpendingActivity extends AppCompatActivity {
         Log.d("amount:",""+newTotal);
         TextView savingAmountTextView = findViewById(R.id.savingAmountTextView);
         TextView savingPercentTextView = findViewById(R.id.savingPercentTextView);
-        savingAmountTextView.setText("Amount saved: "+(ogAmount-newTotal));
+        savingAmountTextView.setText("Amount Saved: $"+(ogAmount-newTotal));
         savingPercentTextView.setText("Percentage Cut: "+Math.round((1-(newTotal/ogAmount)) *100)+"%");
     }
+
+    private void setupBarGraph(){
+        Description d = new Description();
+        d.setText("");
+        barChart.setDescription(d);
+        barChart.getLegend().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
+        List<BarEntry> barEntries = new ArrayList<>();
+        final List<String> labels = new ArrayList<>();
+        float counter = 0;
+        for(Map.Entry<String, TransactionObject> obj : transactionsMap.entrySet()){
+            barEntries.add(new BarEntry(counter,obj.getValue().totalAmount.floatValue()));
+            counter++;
+            labels.add(obj.getKey());
+        }
+        barChart.getXAxis().setGranularityEnabled(true);
+        barChart.getXAxis().setTextColor(Color.GRAY);
+        barChart.getXAxis().setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return labels.get((int) value);
+            }
+        });
+        BarDataSet dataSet = new BarDataSet(barEntries, "");
+        dataSet.setValueTextColor(Color.GRAY);
+        BarData data = new BarData(dataSet);
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        dataSet.setColors(colors);
+
+        barChart.setData(data);
+        barChart.invalidate();
+    }
+
 
     private void setPieGraph(){
         PieChart mChart =  findViewById(R.id.analzyePieChart);
