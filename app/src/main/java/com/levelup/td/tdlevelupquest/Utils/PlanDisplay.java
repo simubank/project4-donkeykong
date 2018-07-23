23e91b9de444de38d5f0df0c6a79122833da60a9
Jason Antao <jasantao99@gmail.com>
2018-07-23 13:44:38 -0400

    Large Feature Update - Savings Plan Screen - Completely Paramaterized Now

---
+++ app/src/main/java/com/levelup/td/tdlevelupquest/Utils/PlanDisplay.java
@@ -0,0 +1,21 @@
+package com.levelup.td.tdlevelupquest.Utils;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.levelup.td.tdlevelupquest.R;
+
+public class PlanDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_display);
+
+    }
+
+}
--- app/src/main/java/com/levelup/td/tdlevelupquest/card_display.java
+++ app/src/main/java/com/levelup/td/tdlevelupquest/card_display.java
@@ -1,7 +1,12 @@
 package com.levelup.td.tdlevelupquest;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+
+import com.levelup.td.tdlevelupquest.Utils.PlanDisplay;
 
 public class card_display extends Activity {
 
@@ -11,6 +16,73 @@ public class card_display extends Activity {
         setContentView(R.layout.activity_card_display);
 
         LaunchActivity.initDrawer(card_display.this, this );
+
+
+        View view1 = findViewById(R.id.card_view_food);
+        View view2 = findViewById(R.id.card_view_electronics);
+        View view3 = findViewById(R.id.card_view_coffee);
+        View view4 = findViewById(R.id.card_view_ride);
+
+        //Food Savings Below
+        view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), PlanDisplay.class);
+
+
+                Resources res = getResources();
+                String text = String.format(res.getString(R.string.title_savings_plan), "Electronics");
+
+
+                intent.putExtra("PLAN_TYPE", "Electronics");
+                intent.putExtra("TOP_COST", "486.69");
+                intent.putExtra("TOTAL_COST", "602.44");
+
+
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //Electronic Savings Below
+        view2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), PlanDisplay.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //Coffee Savings Below
+        view3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), PlanDisplay.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //Ride Savings Below
+        view4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), PlanDisplay.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 
 }
